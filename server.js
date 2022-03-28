86ad5e765eab31bcd650de384e7aa2b6670bea77
Lewis Mocha <mocha@lewis.mocha>
2022-03-28 23:53:59 +0300

    updated server js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://lmocha:Nyangau92@ip1saf.m9xkh.mongodb.net/darkroom?retryWrites=true&w=majority';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) console.log(err)
